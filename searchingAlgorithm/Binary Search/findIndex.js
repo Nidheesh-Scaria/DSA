dcd81bfc2b981ab50f18f2019e8da18182465a49
Nidheesh <nidheeshscariak@gmail.com>
2025-11-28 17:10:34 +0530

    DSA-linear and binary search examples

---
+++ searchingAlgorithm/Binary Search/findIndex.js
@@ -0,0 +1,20 @@
+function indexSearch(arr,target){
+    let start=0
+    let end=arr.length
+    while(start<=end){
+        let middle=Math.floor((start+end)/2)
+        if(arr[middle]===target){
+            return middle
+        }else if(arr[middle]<target){
+            start=middle+1
+        }else{
+            end=middle-1
+        }
+    }
+    return -1
+}
+
+//time complexity -O(log n)
+//space complexity is O(1)
+let arr = [1,2,3,4,5,6,7,8,9];
+console.log(indexSearch(arr,9))
